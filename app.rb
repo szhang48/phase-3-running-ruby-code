c97c8fac3905001d50c0ad454ddd03d7b7c3c2cb
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-11 21:48:43 -0500

    commit tree 2b7cb5d00e5d405ba8deb4ed189c924dae3d689e

---
+++ app.rb
@@ -0,0 +1,27 @@
+# # comments basically the same as  // in  JS 
+
+# # puts "Hello, world!"
+# # puts ("Hello, world!") # this is optional gives the same result
+
+# # puts "Hello, world!"
+# # puts "Hello, world!"
+# # puts "Hello, world!"
+
+# # print "Hello, world!"
+# # print "Hello, world!"
+# # print "Hello, world!"
+
+# puts [1, 2, 3] # might not be the best way in an array
+
+# p [1, 2, 3] #better for arrays
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# #run IRB in terminal
+# #nil is the same as null in JS 
+# #puts method always returns nil 
+
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
